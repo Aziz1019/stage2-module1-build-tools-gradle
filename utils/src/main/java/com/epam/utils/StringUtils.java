20e523ff45b91468877ec45a32bfbe55b0d084af
Aziz1019 <starinc1998@gmail.com>
2023-01-30 14:53:35 +0500

    minor changes

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -6,6 +6,9 @@ public class StringUtils {
             if (Double.parseDouble(str) <= 0) {
                 return false;
             }
+            if(str.startsWith("0") && str.length() > 1) {
+                return false;
+            }
         } catch (NumberFormatException | NullPointerException e) {
             return false;
         }
