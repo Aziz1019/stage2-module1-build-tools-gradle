25a48b15e025e5cd22af9aea3bef898257300fc4
Aziz1019 <starinc1998@gmail.com>
2023-01-30 15:31:33 +0500

    minor changes

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -3,11 +3,10 @@ package com.epam.utils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
-            if (Integer.parseInt(str) > 0){
+            if (Double.parseDouble(str) <= 0) {
                 return false;
             }
-
-            if (str.startsWith("0") && str.length() > 1) {
+            if(str.startsWith("0") && str.length() > 1) {
                 return false;
             }
         } catch (NumberFormatException | NullPointerException e) {
